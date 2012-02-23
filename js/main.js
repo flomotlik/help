60c5d9bc5bfba8290fcd83baa3ffdb4689d133e9
Florian Motlik <flomotlik@gmail.com>
2012-02-23 15:08:37 +0100

    make accordion collapsible

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(function() {
   var enabled = Math.round($("li.disable").parent().prevAll().length / 2) - 1;
-  $(".accordion").accordion({active : enabled});
+  $(".accordion").accordion({active : enabled, collapsible : true});
 });
